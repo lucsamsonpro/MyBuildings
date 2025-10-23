925e28001b7a68309de46c80b3195b5189df8266
lucsamsonpro <luc.samson.pro@gmail.com>
2025-10-23 11:37:44 +0200

    SetUp Project
    Import project + SetUp LSF

---
+++ MyBuildings/Assets/QuitApplication.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuitApplication : MonoBehaviour {
+
+    public void quit() {
+        Application.Quit();
+        Debug.Log ("Rage Quited");
+    }
+}
